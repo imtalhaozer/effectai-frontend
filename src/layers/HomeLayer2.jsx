7798a8995390e54715c15fb0388d6ad0ef41e506
imtalhaozer <imtalhaozer@gmail.com>
2024-09-03 22:25:08 +0300

    homelayer section created and animation added

--- src/layers/HomeLayer2.jsx
+++ src/layers/HomeLayer2.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
-export const HomeLayer2 = () => {
+import { Navbar } from '../components/Navbar';
+import { motion } from 'framer-motion';
+export const HomeLayer2 = ({layer2}) => {
+    const color1='#000000'
+    const color2='#FFFFFF'
   return (
-    <div>HomeLayer2</div>
+    <div className='h-screen w-screen bg-white'>
+      <div className='flex flex-col items-center justify-center'>
+          <motion.div
+          initial={{y:'0%'}}
+          animate={layer2?{y:"50%"}:{}}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          >
+              hello world
+          </motion.div>
+      </div>
+    </div>
   )
 }
