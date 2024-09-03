a816b9059eaf088150fbdc7647e0df50d7ad7ad6
imtalhaozer <imtalhaozer@gmail.com>
2024-09-03 22:21:19 +0300

    HomePage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomeLayer1} from './layers/HomeLayer1'
+import { HomeLayer2} from './layers/HomeLayer2'
+import {HomePage} from './pages/HomePage'
 function App (){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomeLayer1/>} />
+        <Route path='/2' element={<HomeLayer2/>} />
+        <Route path='/1' element={<HomeLayer1/>} />
+        <Route path='/' element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   )
